3ddb0f92cd7d654f2e689e7b707abe8591d2f338
Wok <woctezuma@users.noreply.github.com>
2023-07-29 17:56:11 +0200

    Minor refactoring: define main()

--- extract_fts.py
+++ extract_fts.py
@@ -16,7 +16,8 @@ from src.transform_utils import get_transform
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-if __name__ == '__main__':
+
+def main():
     params = get_parser().parse_args()
     print(f"__log__:{json.dumps(vars(params))}")
 
@@ -47,3 +48,7 @@ if __name__ == '__main__':
     print('>>> Saving features...')
     features = torch.concat(features, dim=0)
     torch.save(features, Path(params.output_dir) / 'fts.pth')
+
+
+if __name__ == '__main__':
+    main()
