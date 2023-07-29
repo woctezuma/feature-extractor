6d72925d07ed090a2ebf3a3861d42057d9989142
Wok <woctezuma@users.noreply.github.com>
2023-07-29 18:17:47 +0200

    Minor refactoring: move ImageFolder in a separate file

--- src/data_utils.py
+++ src/data_utils.py
@@ -4,47 +4,9 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import functools
-import logging
-import os
-from pathlib import Path
-
-from PIL import ImageFile
 from torch.utils.data import DataLoader
-from torchvision.datasets.folder import default_loader, is_image_file
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-
-# Data loading
-
-
-@functools.lru_cache
-def get_image_paths(path):
-    logging.info(f"Resolving files in: {path}")
-    paths = []
-    for _dirpath, _dirnames, filenames in os.walk(path):
-        paths.extend([Path(path) / filename for filename in filenames])
-    return sorted([fn for fn in paths if is_image_file(fn)])
-
-
-class ImageFolder:
-    """An image folder dataset without classes"""
-
-    def __init__(self, path, transform=None, loader=default_loader):
-        self.samples = get_image_paths(path)
-        self.loader = loader
-        self.transform = transform
-
-    def __getitem__(self, idx: int):
-        assert 0 <= idx < len(self)
-        img = self.loader(self.samples[idx])
-        if self.transform:
-            return self.transform(img)
-        return img
-
-    def __len__(self):
-        return len(self.samples)
+from src.image_folder import ImageFolder
 
 
 def collate_fn(batch):
---
+++ src/image_folder.py
@@ -0,0 +1,37 @@
+import functools
+import logging
+import os
+from pathlib import Path
+
+from PIL import ImageFile
+from torchvision.datasets.folder import default_loader, is_image_file
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+@functools.lru_cache
+def get_image_paths(path):
+    logging.info(f"Resolving files in: {path}")
+    paths = []
+    for _dirpath, _dirnames, filenames in os.walk(path):
+        paths.extend([Path(path) / filename for filename in filenames])
+    return sorted([fn for fn in paths if is_image_file(fn)])
+
+
+class ImageFolder:
+    """An image folder dataset without classes"""
+
+    def __init__(self, path, transform=None, loader=default_loader):
+        self.samples = get_image_paths(path)
+        self.loader = loader
+        self.transform = transform
+
+    def __getitem__(self, idx: int):
+        assert 0 <= idx < len(self)
+        img = self.loader(self.samples[idx])
+        if self.transform:
+            return self.transform(img)
+        return img
+
+    def __len__(self):
+        return len(self.samples)
