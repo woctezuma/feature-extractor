aee2bc9a61be166a25d7a090b59d3db04f98679c
Wok <woctezuma@users.noreply.github.com>
2023-07-29 17:53:13 +0200

    Refactoring: add utils for parser

--- extract_fts.py
+++ extract_fts.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import argparse
 import json
 from pathlib import Path
 
@@ -12,60 +11,12 @@ import torch
 import tqdm
 
 from src import utils
+from src.parser_utils import get_parser
 from src.transform_utils import get_transform
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
-
-    def get_parser():
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-            "--output_dir",
-            type=str,
-            default='features',
-            help="The path to the output folder where features will be saved.",
-        )
-        parser.add_argument(
-            "--data_dir",
-            type=str,
-            default="images",
-            help="The path to the input folder where images are stored.",
-        )
-        parser.add_argument(
-            "--model_repo",
-            type=str,
-            default="facebookresearch/dinov2",
-            help="A github repo with format `repo_owner/repo_name`, for example ‘pytorch/vision’.",
-        )
-        parser.add_argument(
-            "--model_name",
-            type=str,
-            default="dinov2_vits14",
-            help="The name of a callable (entrypoint) defined in the repo’s hubconf.py.",
-        )
-        parser.add_argument(
-            "--resize_size",
-            type=int,
-            default=256,
-            help="Desired image output size after the resize.",
-        )
-        parser.add_argument(
-            "--keep_ratio",
-            type=bool,
-            default=True,
-            help="Whether to keep the image ratio: the smallest image side will match `resize_size`.",
-        )
-        parser.add_argument(
-            "--crop_size",
-            type=int,
-            default=224,
-            help="Desired image output size after the center-crop.",
-        )
-        parser.add_argument("--batch_size", type=int, default=256, help="Batch size.")
-
-        return parser
-
     params = get_parser().parse_args()
     print(f"__log__:{json.dumps(vars(params))}")
 
---
+++ src/parser_utils.py
@@ -0,0 +1,50 @@
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default='features',
+        help="The path to the output folder where features will be saved.",
+    )
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default="images",
+        help="The path to the input folder where images are stored.",
+    )
+    parser.add_argument(
+        "--model_repo",
+        type=str,
+        default="facebookresearch/dinov2",
+        help="A github repo with format `repo_owner/repo_name`, for example ‘pytorch/vision’.",
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="dinov2_vits14",
+        help="The name of a callable (entrypoint) defined in the repo’s hubconf.py.",
+    )
+    parser.add_argument(
+        "--resize_size",
+        type=int,
+        default=256,
+        help="Desired image output size after the resize.",
+    )
+    parser.add_argument(
+        "--keep_ratio",
+        type=bool,
+        default=True,
+        help="Whether to keep the image ratio: the smallest image side will match `resize_size`.",
+    )
+    parser.add_argument(
+        "--crop_size",
+        type=int,
+        default=224,
+        help="Desired image output size after the center-crop.",
+    )
+    parser.add_argument("--batch_size", type=int, default=256, help="Batch size.")
+
+    return parser
