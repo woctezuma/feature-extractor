76bab6d63a4ca7c64e17a758d3b8cdbab7b31754
Wok <woctezuma@users.noreply.github.com>
2023-07-31 12:39:59 +0200

    Try to fix TypeError: Unexpected type numpy.ndarray

--- src/transform_utils.py
+++ src/transform_utils.py
@@ -1,9 +1,27 @@
+import torch
 from torchvision import transforms
 
 # Reference: https://github.com/facebookresearch/dinov2/blob/main/dinov2/data/transforms.py
 
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
+
+
+class MaybeToTensor(transforms.ToTensor):
+    """
+    Convert a ``PIL Image`` or ``numpy.ndarray`` to tensor, or keep as is if already a tensor.
+    """
+
+    def __call__(self, pic):
+        """
+        Args:
+            pic (PIL Image, numpy.ndarray or torch.tensor): Image to be converted to tensor.
+        Returns:
+            Tensor: Converted image.
+        """
+        if isinstance(pic, torch.Tensor):
+            return pic
+        return super().__call__(pic)
 
 
 def get_target_image_size(resize_size=256, keep_ratio=True):
@@ -22,7 +40,7 @@ def get_transform(
             interpolation=interpolation,
         ),
         transforms.CenterCrop(crop_size),
-        transforms.ToTensor(),
+        MaybeToTensor(),
         transforms.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
     ]
     return transforms.Compose(transforms_list)
