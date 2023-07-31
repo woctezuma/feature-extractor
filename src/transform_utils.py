8b4aeb6f0d0503ed48b54f4814b0fa59b7d45102
Wok <woctezuma@users.noreply.github.com>
2023-07-31 12:47:31 +0200

    Add a reference for the code

--- src/transform_utils.py
+++ src/transform_utils.py
@@ -1,5 +1,7 @@
 from torchvision import transforms
 
+# Reference: https://github.com/facebookresearch/dinov2/blob/main/dinov2/data/transforms.py
+
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 
